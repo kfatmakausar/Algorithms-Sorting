5a3699897b2c8249b0848d66950fb06a58b0f252
Fatma Kausar <kfatmakausar@gmail.com>
2020-09-08 19:01:41 -0400

    Initial commit

---
+++ Algorithms Lab1/Source.cpp
@@ -0,0 +1,14 @@
+///@file Source.cpp
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+
+	cout << "Hello world!" << endl;
+
+	system("PAUSE");
+	return 0;
+} //end main
