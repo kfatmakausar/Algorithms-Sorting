4fdcb3c399e5cd481c7a32a76a0b2752786cfa12
Fatma Kausar <kfatmakausar@gmail.com>
2020-09-16 19:01:25 -0400

    Added c approximation and cleaned up code

--- Algorithms Lab1/Source.cpp
+++ Algorithms Lab1/Source.cpp
@@ -6,10 +6,12 @@
 #include <vector>
 #include <algorithm> //for random_shuffle
 #include <chrono> //for chronotime
+#include <math.h> // for pow and log
 using namespace std;
 
 long long int steps=0;
 long long int stepCount=0;
+long double cApprox = 0;
 
 //InsertionSort algorithm
 void insertionSort(vector<int> &arr)
@@ -162,98 +164,114 @@ void generateRandom(vector<int> &arr, int n)
 
 int main() 
 {
-	// Array of inputs
+	// Array of inputs n
 	int inputs[8] = { 100, 200, 300, 400, 500, 1000, 4000, 10000 };
 	
 	//InsertionSort for Sorted Arrays, Reverse Arrays, Random Permutation Arrays
+	//C approximation will be stepCount/n^2
 	cout << "---------------------------------INSERTION SORT----------------------------------------" << endl;
 	cout << "SORTED ARRAYS" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
 		stepCount = 0;
+		cApprox = 0;
 		vector<int> arr;
 		generateSort(arr, inputs[i]);
 		auto t1 = chrono::high_resolution_clock::now();
 		insertionSort(arr);
 		auto t2 = chrono::high_resolution_clock::now();
-		chrono::duration<double, milli> elapsed = t2 - t1;
-		cout << arr.size() << " ELEMENTS: Steps = " << stepCount << " | Runtime = " << elapsed.count() << " miliseconds." << endl;
+		chrono::duration<double, milli> timer = t2 - t1;
+		cApprox = (stepCount) / (pow(inputs[i], 2));
+		cout << arr.size() << " ELEMENTS: Steps = " << stepCount << " | Runtime = " << timer.count() << " miliseconds. | C Approximation: " << cApprox << endl;
 		arr.clear();
 	}
 	cout << "REVERSED ARRAYS" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
 		stepCount = 0;
+		cApprox = 0;
 		vector<int> arr;
 		generateReverse(arr, inputs[i]);
 		auto t1 = chrono::high_resolution_clock::now();
 		insertionSort(arr);
 		auto t2 = chrono::high_resolution_clock::now();
-		chrono::duration<double, milli> elapsed = t2 - t1;
-		cout << arr.size() << " ELEMENTS: Steps = " << stepCount << " | Runtime = " << elapsed.count() << " miliseconds." << endl;
+		chrono::duration<double, milli> timer = t2 - t1;
+		cApprox = (stepCount) / (pow(inputs[i], 2));
+		cout << arr.size() << " ELEMENTS: Steps = " << stepCount << " | Runtime = " << timer.count() << " miliseconds. | C Approximation: " << cApprox << endl;
 		arr.clear();
 	}
 	cout << "RANDOM PERMUTATION ARRAYS" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
 		stepCount = 0;
+		cApprox = 0;
 		vector<int> arr;
 		generateRandom(arr, inputs[i]);
 		auto t1 = chrono::high_resolution_clock::now();
 		insertionSort(arr);
 		auto t2 = chrono::high_resolution_clock::now();
-		chrono::duration<double, milli> elapsed = t2 - t1;
-		cout << arr.size() << " ELEMENTS: Steps = " << stepCount << " | Runtime = " << elapsed.count() << " miliseconds." << endl;
+		chrono::duration<double, milli> timer = t2 - t1;
+		cApprox = (stepCount) / (pow(inputs[i], 2));
+		cout << arr.size() << " ELEMENTS: Steps = " << stepCount << " | Runtime = " << timer.count() << " miliseconds. | C Approximation: " << cApprox << endl;
 		arr.clear();
 	}
 	
 	//MergeSort for Sorted Arrays, Reverse Arrays, Random Permutation Arrays
+	//C approximation will be steps/n*log(n)
 	cout << endl << endl << "---------------------------------MERGE SORT----------------------------------------" << endl;
 	cout << "SORTED ARRAYS" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
 		steps = 0;
+		cApprox = 0;
 		vector<int> arr;
 		generateSort(arr, inputs[i]);
 		auto t1 = chrono::high_resolution_clock::now();
 		mergeSort(arr, 0, (arr.size() - 1));
 		auto t2 = chrono::high_resolution_clock::now();
-		chrono::duration<double, milli> elapsed = t2 - t1;
-		cout << arr.size() << " ELEMENTS: Steps = " << steps << " | Runtime = " << elapsed.count() << " miliseconds." << endl;
+		chrono::duration<double, milli> timer = t2 - t1;
+		cApprox = (steps) / (inputs[i] * log(inputs[i]));
+		cout << arr.size() << " ELEMENTS: Steps = " << steps << " | Runtime = " << timer.count() << " miliseconds. | C Approximation: " << cApprox << endl;
 		arr.clear();
 	}
 	cout << "REVERSED ARRAYS" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
 		steps = 0;
+		cApprox = 0;
 		vector<int> arr;
 		generateReverse(arr, inputs[i]);
 		auto t1 = chrono::high_resolution_clock::now();
 		mergeSort(arr, 0, (arr.size() - 1));
 		auto t2 = chrono::high_resolution_clock::now();
-		chrono::duration<double, milli> elapsed = t2 - t1;
-		cout << arr.size() << " ELEMENTS: Steps = " << steps << " | Runtime = " << elapsed.count() << " miliseconds." << endl;
+		chrono::duration<double, milli> timer = t2 - t1;
+		cApprox = (steps) / (inputs[i] * log(inputs[i]));
+		cout << arr.size() << " ELEMENTS: Steps = " << steps << " | Runtime = " << timer.count() << " miliseconds. | C Approximation: " << cApprox << endl;
 		arr.clear();
 	}
 	cout << "RANDOM PERMUTATION ARRAYS" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
 		steps = 0;
+		cApprox = 0;
 		vector<int> arr;
 		generateRandom(arr, inputs[i]);
 		auto t1 = chrono::high_resolution_clock::now();
 		mergeSort(arr, 0, (arr.size() - 1));
 		auto t2 = chrono::high_resolution_clock::now();
-		chrono::duration<double, milli> elapsed = t2 - t1;
-		cout << arr.size() << " ELEMENTS: Steps = " << steps << " | Runtime = " << elapsed.count() << " miliseconds." << endl;
+		chrono::duration<double, milli> timer = t2 - t1;
+		cApprox = (steps) / (inputs[i] * log(inputs[i]));
+		cout << arr.size() << " ELEMENTS: Steps = " << steps << " | Runtime = " << timer.count() << " miliseconds. | C Approximation: " << cApprox << endl;
 		arr.clear();
 	}
 
+	cout << endl << endl << "---------------------------------50 INSTANCES----------------------------------------" << endl;
 	//50 instances of Random inputs InsertionSort
 	cout << "50 INSTANCES INSERTION SORT" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
 		stepCount = 0;
+		cApprox = 0;
 		double totalT = 0;
 		for (int x = 0; x < 50; x++)
 		{
@@ -265,11 +283,12 @@ int main()
 			auto t1 = chrono::high_resolution_clock::now();
 			insertionSort(arr);
 			auto t2 = chrono::high_resolution_clock::now();
-			chrono::duration<double, milli> elapsed = t2 - t1;
-			totalT += elapsed.count();
+			chrono::duration<double, milli> timer = t2 - t1;
+			totalT += timer.count();
 			arr.clear();
 		}
-		cout << "50 INSTANCES OF " << inputs[i] << " ELEMENTS: Steps = " << stepCount << " | Runtime = " << totalT << " miliseconds | Average time: " << totalT/50 << " miliseconds." << endl;
+		cApprox = (stepCount / 50) / (pow(inputs[i],2));
+		cout << "50 INSTANCES OF " << inputs[i] << " ELEMENTS: Steps = " << stepCount << " | Runtime = " << totalT << " miliseconds | Average time: " << totalT/50 << " miliseconds. | C Approximation: " << cApprox << endl;
 	}
 
 	//50 instances of random inputs mergeSort
@@ -277,6 +296,7 @@ int main()
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
 		steps = 0;
+		cApprox = 0;
 		double totalT = 0;
 		for (int x = 0; x < 50; x++)
 		{
@@ -288,31 +308,15 @@ int main()
 			auto t1 = chrono::high_resolution_clock::now();
 			mergeSort(arr, 0, (arr.size() - 1));
 			auto t2 = chrono::high_resolution_clock::now();
-			chrono::duration<double, milli> elapsed = t2 - t1;
-			totalT += elapsed.count();
+			chrono::duration<double, milli> timer = t2 - t1;
+			totalT += timer.count();
 			arr.clear();
 		}
-		cout << "50 INSTANCES OF " << inputs[i] << " ELEMENTS: Steps = " << steps << " | Runtime = " << totalT << " miliseconds | Average time: " << totalT/50 << " miliseconds." << endl;
+		cApprox = (steps / 50) / (inputs[i] * log(inputs[i]));
+		cout << "50 INSTANCES OF " << inputs[i] << " ELEMENTS: Steps = " << steps << " | Runtime = " << totalT << " miliseconds | Average time: " << totalT/50 << " miliseconds. | C Approximation: " << cApprox << endl;
 	}
 	
 	
-	
-	//CANVA FOR GRAPH DISPLAY
-
-	/*
-	She said that the number of steps/ n will give u the constant approximation
-	steps/n = c
-	c *n^2 for insertion ... nlogn for merge
-	make steps global variable and reset it later
-
-	steps / n^2
-	steps / n(logn)
-	*/
-
-	
-	
-	
-
 	system("PAUSE");
 	return 0;
 } //end main
