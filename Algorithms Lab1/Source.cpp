d01ba5db10629eea8dc0c57cbecf50166f609fd7
Fatma Kausar <kfatmakausar@gmail.com>
2020-09-16 17:27:23 -0400

    Clean up code

--- Algorithms Lab1/Source.cpp
+++ Algorithms Lab1/Source.cpp
@@ -164,7 +164,8 @@ int main()
 {
 	// Array of inputs
 	int inputs[8] = { 100, 200, 300, 400, 500, 1000, 4000, 10000 };
-	/*
+	
+	//InsertionSort for Sorted Arrays, Reverse Arrays, Random Permutation Arrays
 	cout << "---------------------------------INSERTION SORT----------------------------------------" << endl;
 	cout << "SORTED ARRAYS" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
@@ -206,6 +207,7 @@ int main()
 		arr.clear();
 	}
 	
+	//MergeSort for Sorted Arrays, Reverse Arrays, Random Permutation Arrays
 	cout << endl << endl << "---------------------------------MERGE SORT----------------------------------------" << endl;
 	cout << "SORTED ARRAYS" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
@@ -246,9 +248,8 @@ int main()
 		cout << arr.size() << " ELEMENTS: Steps = " << steps << " | Runtime = " << elapsed.count() << " miliseconds." << endl;
 		arr.clear();
 	}
-	*/
 
-	
+	//50 instances of Random inputs InsertionSort
 	cout << "50 INSTANCES INSERTION SORT" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
@@ -271,6 +272,7 @@ int main()
 		cout << "50 INSTANCES OF " << inputs[i] << " ELEMENTS: Steps = " << stepCount << " | Runtime = " << totalT << " miliseconds | Average time: " << totalT/50 << " miliseconds." << endl;
 	}
 
+	//50 instances of random inputs mergeSort
 	cout << "50 INSTANCES MERGE SORT" << endl;
 	for (int i = 0; i < sizeof(inputs) / sizeof(inputs[0]); i++)
 	{
@@ -294,124 +296,10 @@ int main()
 	}
 	
 	
-
-
-
-	/*
-	PRINTING THE VECTOR ARRAYS
-	for (int x : reverse500)
-	{
-		cout << x << " ";
-	}
-	*/
 	
 	//CANVA FOR GRAPH DISPLAY
-	
-	//count steps in merge helper function and mergesort and add them , u pass steps by reference into the functions and make steps a global variable
-	
-	
-	//for loops to initialize arrays with n size
-	/*
-	Create a vector/dynamic array and then create another array with n's and populate them with the array of n
-	int n = { 100, 200, 300 }
-	This way u can loop over every n 
-	arr.clear() to delete the vector array
-	delete[] to delete the dynamic array
-	If u will use vectors, remember that vectors ARE NOT sent by reference so it would be vector<int>&
-	*/
 
 	/*
-	INSERTION SORT CODE
-	int i, key, j;
-
-    for (i = 1; i < arr.size(); i++) {
-        key = arr[i];
-        j = i - 1;
-
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-
-        arr[j + 1] = key;
-
-    }
-	*/
-
-	/*
-	ANOTHER WAY TO WRITE INSERTION SORT CODE
-	void insertionSort(vector<int>& vec)
-	{
-    int steps = 0;
-    for (auto it = vec.begin(); it != vec.end(); it++)
-    {
-
-        // Searching the upper bound, i.e., first
-        // element greater than *it from beginning 
-        auto const insertion_point =
-            upper_bound(vec.begin(), it, *it);
-
-
-        // Shifting the unsorted part 
-        rotate(insertion_point, it, it + 1);
-
-    }
-
-	}*/
-
-	/*
-	ONE WAY TO GET RUNTIME
-	clock_t time_req;
-	time_req = clock();
-	insertionSort(arr);
-	time_req = clock() - time_req;
-
-	ANOTHER WAY TO GET RUNTIME
-	#include <chrono>
-	auto t1 = chrono::high_resolution_clock::now();
-	merge_sort(v2, 0, n2 - 1);
-	auto t2 = chrono::high_resolution_clock::now();
-	chrono::duration<double, milli> elapsed = t2 - t1;
-
-	cout << "Algorithm Runtime is: " << elapsed.count() << " miliseconds.";
-	*/
-
-	/*
-	//Process time of insertion sort of 15000 numbers: 4.565
-	//Process time of merge sort of 15000 numbers: 0.229
-	//Process time of insertion sort of 50 instances of 10000 numbers: 5109.78
-	*/
-
-	/*
-	else if (mode == 3) {
-	//(3) random
-	for (int i = 0; i < n; i++) {
-		arr.push_back(i);
-	}
-	random_shuffle(arr.begin(), arr.end());
-	*/
-
-	/*
-	TIMERS 
-	//this is me initializing the timers
-	auto t1 = chrono::high_resolution_clock::now();
-	insertionSort(arr1, n1);
-	auto t2 = chrono::high_resolution_clock::now();
-
-	//this is me reusing the timers
-	t1 = chrono::high_resolution_clock::now();
-	insertionSort(arr2, n2);
-	t2 = chrono::high_resolution_clock::now();
-	*/
-
-	/*
-	random permutations - no duplicates - use random shuffle
-	50 instances - duplicates are fine - can use rand - rand()%n+1
-	*/
-
-	/*
-	for showing steps, use a counter to count each statement
-	for everything u see inside insertion/sort just add steps++
 	She said that the number of steps/ n will give u the constant approximation
 	steps/n = c
 	c *n^2 for insertion ... nlogn for merge
@@ -421,24 +309,7 @@ int main()
 	steps / n(logn)
 	*/
 
-	/*
-	(4)
-	Just create one array and keep sorting and shuffling it for 50 times
-	Thats what I did and it works fine
-		//While loop for the 50 instances
-	//Create a while loop that goes 50 times
-	//	Inside the loop
-	//	U initialize the vector using rand() % n + 1
-
-	create 50 arrays filled with N numbers randomly, sort them, and calculate the average
-	Yeah, first random, then the average of sorting 50 arrays of size N
-
-	I ran a loop 50 times. In the loop I initialized a temp array of size N, 
-	then created a nested loop to populate the temp array with random values from 1 to N, 
-	then when it's populated, I measured start time, then run algorithm, then measured end time, 
-	then I add the difference between end and start time to the total time variable. 
-	After loop runs 50 times I divide the total time by 50 to get the average.
-	*/
+	
 	
 	
 
